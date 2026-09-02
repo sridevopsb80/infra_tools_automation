01eaf7cc86f866debceb9ac671dfb2e21a831782
srikvp <93551165+srikvp@users.noreply.github.com>
2026-09-02 16:16:26 -0400

    update run.sh

--- ansible-run.sh
+++ ansible-run.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 # $1 value can be the ip address or the dns name of the server we wish to connect to
 
+set -e
+
+HOST="$1"
 
 # shellcheck disable=SC2296
-ansible-playbook -i $1, \
+ansible-playbook -i "${HOST}", \
     tool-setup.yml -e ansible_user=ec2-user \
     -e ansible_password=DevOps321 \
     -e tool_name=github-runner
